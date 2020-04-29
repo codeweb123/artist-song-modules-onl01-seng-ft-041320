0eaf2187aadf06cf2c4ce7521ecbf2e6400d922f
Jiyon FL <jiyonj@hotmail.com>
2020-04-29 23:21:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ require 'pry'
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  
   extend Findable
   include Paramable
+
   attr_accessor :name
   attr_reader :artist
 
