8de4e902653da2eb392d856db764d1c40b701cd9
Jiyon FL <jiyonj@hotmail.com>
2020-04-29 23:16:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,30 @@
 require 'pry'
 
-class Song
+class Artist
+  attr_accessor :name
+  attr_reader :songs
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  attr_accessor :name
-  attr_reader :artist
 
-  @@songs = []
+  @@artists = []
 
-  def artist=(artist)
-    @artist = artist
+  def initialize
+    super
+    @songs = []
   end
 
   def self.all
-    @@songs
+    @@artists
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
   end
 end
